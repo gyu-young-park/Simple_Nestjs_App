011c7157335da00b586d316497e902439d9b6b2e
gyu-young-park <gyoue200125@gmail.com>
2021-08-24 02:02:25 +0900

    connected postgre server and add typeorm , @nestjs/typeorm and repository code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { BoardsService } from './boards/boards.service';
+import { typeORMConfig } from './configs/typeorm.config'
+
 @Module({
-  imports: [BoardsModule],
-  providers: [BoardsService]
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+  providers: [BoardsService],
 })
 export class AppModule {}
